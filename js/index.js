64eb3ed714e3f99e4c02b8e5214c5197276c4f32
Jie Huang <nattime@gmail.com>
2017-03-08 21:25:12 -0500

    fix index footer

--- js/index.js
+++ js/index.js
@@ -10,12 +10,13 @@ window.onload = function(){
 		var nav = document.getElementById("nav");
 		var main_info_height = document.getElementsByClassName("main-column")[0].offsetHeight;
 		var nav_height = nav.offsetHeight;
+		var footer_height = footer_info[0].offsetHeight;
 
 		// alert(nav_height);
 		logo_img[0].style.margin = "0px " + ((w - 150) / 2) + "px";
 		// logo_img[0].style.bottom = -75 + "px";
 		nav.style.margin = "0px " + ((w - 200) / 2) + "px";
 		nav.style.bottom = -(main_info_height + nav_height + 5) + "px";
-		footer_info[0].style.margin = (nav_height + 10) + "px 0px 0px 0px"
+		footer_info[0].style.margin = (h - footer_height + 10) + "px 0px 0px 0px"
 	}
 }

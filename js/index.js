b648a23326ab162c2c15dee629667c21069b5aaf
Jie Huang <nattime@gmail.com>
2017-03-08 20:58:19 -0500

    fix index 480px

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ window.onload = function(){
 		var footer_info = document.getElementsByClassName("footer-info");
 		var logo_img = document.getElementsByClassName("logo-150px");
 		var nav = document.getElementById("nav");
-		var main_info_height = document.getElementsByClassName("main-info")[0].offsetHeight;
+		var main_info_height = document.getElementsByClassName("main-column")[0].offsetHeight;
 
 		// alert(main_info_height);
 		logo_img[0].style.margin = "0px " + ((w - 150) / 2) + "px";
-		logo_img[0].style.bottom = -75 + "px";
-		nav.style.margin = main_info_height + "px " + ((w - 200) / 2) + "px";
+		// logo_img[0].style.bottom = -75 + "px";
+		nav.style.margin = main_info_height + "px " + ((w - 200) / 2) + "px " + "0px " + ((w - 200) / 2) + "px";
 	}
 }

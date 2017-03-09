0f4c3937bbd036912b4bed7d21883bf4573dcf7e
Jie Huang <nattime@gmail.com>
2017-03-08 20:21:47 -0500

    fix index 480px

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	// alert(h);
+	// alert(w);
+	if(w < 480){
+		// alert(w);
+		var footer_info = document.getElementsByClassName("footer-info");
+		
+
+	}
+}

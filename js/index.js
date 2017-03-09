3d028ee7dc0339682baa61ab7e387edb85dc1723
Jie Huang <nattime@gmail.com>
2017-03-08 20:31:24 -0500

    fix index 480px

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@ window.onload = function(){
 	if(w < 480){
 		// alert(w);
 		var footer_info = document.getElementsByClassName("footer-info");
-		
+		var logo_img = document.getElementsByClassName("logo-150px");
 
+		logo_img[0].style.margin = "0px " + ((w - 150) / 2) + "px";
 	}
 }
